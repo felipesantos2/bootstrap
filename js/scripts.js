a0ceaab479f7ad5dd480085f826e953de28000ff
Felipe <santospinheiro627@gmail.com>
2022-03-21 22:17:27 -0300

    validando campo email e mensagem

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,42 @@ $('#form-contato').submit(function () {
 
     
     var nome = $('#nome');
+    var email = $('#email');
+    var mensagem = $('#mensagem');
     var erros = $('.alert');
     var campo = $('#campo-erro');
 
 
-    
+    // escondendo a mensagem de erro
     erros.addClass('d-none');
+    $('.is-invalid').removeClass('is-invalid');
 
+
+    // Valida o campo Nome
     if (nome.val() == '') {
         erros.removeClass('d-none');
         campo.html('nome!');  // campo não preenchido
+        nome.focus();
+        nome.addClass('is-invalid'); 
+    return false;
+
+    };
+
+    // Valida o Campo E-mail
+    if (email.val() == '') {
+        erros.removeClass('d-none');
+        campo.html('E-mail!');  // campo não preenchido
+        nome.focus();
+        nome.addClass('is-invalid'); 
+    return false;
+
+    };
+    // Valida o campo Mensagem
+    if (mensagem.val() == '') {
+        erros.removeClass('d-none');
+        campo.html('Mensagem!');  // campo não preenchido
+        nome.focus();
+        nome.addClass('is-invalid'); 
     return false;
 
     };
