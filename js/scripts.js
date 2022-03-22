de7bea228a669b3a9e4d16bc18c65ad98014976c
Felipe <santospinheiro627@gmail.com>
2022-03-21 21:48:27 -0300

    validando o formulario com jquery

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,22 @@ $('#form-contato').submit(function () {
     
     // alert('Clicou no enviar');
 
+    
+    var nome = $('#nome');
+    var erros = $('.alert');
+    var campo = $('#campo-erro');
+
+
+    
+    erros.addClass('d-none');
+
+    if (nome.val() == '') {
+        erros.removeClass('d-none');
+        campo.html('nome!');  // campo não preenchido
+    return false;
+
+    };
+
     return false;
 
 })
